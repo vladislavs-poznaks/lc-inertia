9c0aea136a788f9ca87ae3dce803baa046127045
Vladislavs <vladislavs@Vladislavss-iMac.local>
2021-10-07 19:22:05 +0300

    Episode 2

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>Hello, {{ name }}</h1>
+    <p>In this series we will use the following frameworks:</p>
+
+    <ul>
+        <li v-for="framework of frameworks" v-text="framework"></li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props: {
+        name: String,
+        frameworks: Array,
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return inertia('Home', [
+        'name' => 'Vlad',
+        'frameworks' => [
+            'Laravel', 'Vue', 'Inertia'
+        ]
+    ]);
 });
